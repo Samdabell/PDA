d71d29c33401fb6597f4ea5b562e557bdcc5ebe2
Sam DaBell <sam.dabell@gmail.com>
2017-06-02 11:36:14 +0100

    Added and corrected practical_task using a spec file

---
+++ static_and_dynamic_testing/practical_task_spec.rb
@@ -0,0 +1,28 @@
+require 'minitest/autorun'
+require 'minitest/rg'
+require_relative 'practical_task'
+
+class TestPracticalTask < MiniTest::Test
+
+  def test_func1_true
+    assert_equal(true, func1(1))
+  end
+
+  def test_func1_false
+    assert_equal(false, func1(2))
+  end
+
+  def test_max_a_larger_b
+    assert_equal(10, max(10, 2))
+  end
+
+  def test_max_b_larger_a
+    assert_equal(5, max(1,5))
+  end
+
+  def test_looper
+    expected = 1..10
+    assert_equal(expected, looper)    
+  end
+
+end
